4f6b17c69518059012df31b773bc537be3019e48
ChocolateXmas <alex.beigel@gmail.com>
2025-03-05 19:52:54 +0200

    Refactor(user_manage, user_del): moved all user delete section from user_manage.sh to user_del.sh

--- scripts/userManage/userActions/delete/user_del.sh
+++ scripts/userManage/userActions/delete/user_del.sh
@@ -2,3 +2,25 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../../../config/config.sh"
 source "$PROJECT_ROOT/scripts/userManage/user_utils.sh"
+
+user_del() {
+    while true; do
+        echo "$cancelTitle"
+        read -p "Which User To Delete? > " usr2del
+        if cancelRead "$usr2del" ; then break; fi
+        if ! validateUserData "$usr2del" ; then continue; fi
+        read -p "Are you sure you want to DELETE '$usr2del'? (y/N)" confirm
+        if [[ "$confirm" =~ ^[Yy]$ ]]; then
+            sudo userdel -r "$usr2del"
+            if [[ $? -ne 0 ]]; then
+                echo "User Delete FAILED !"
+                break
+            fi
+            echo "$usr2del has been DELETED !"
+            break
+        else
+            echo -e "User deletion has been CANCLED !\nNo Changes were made"
+            break
+        fi
+    done
+}
--- scripts/userManage/user_manage.sh
+++ scripts/userManage/user_manage.sh
@@ -38,25 +38,7 @@ UserManageMenu() {
 			# Delete User
 			"3")
 				printUserList
-				while true; do
-				    echo "$cancelTitle"
-					read -p "Which User To Delete? > " usr2del
-					if cancelRead "$usr2del" ; then break; fi
-					if ! validateUserData "$usr2del" ; then continue; fi
-					read -p "Are you sure you want to DELETE '$usr2del'? (y/N)" confirm
-					if [[ "$confirm" =~ ^[Yy]$ ]]; then
-						sudo userdel -r "$usr2del"
-						if [[ $? -ne 0 ]]; then
-						    echo "User Delete FAILED !"
-						    break
-						fi
-						echo "$usr2del has been DELETED !"
-						break
-					else
-						echo -e "User deletion has been CANCLED !\nNo Changes were made"
-						break
-					fi
-				done
+				user_del
 				;;
 			# Modify User's Properties
 			"4")
